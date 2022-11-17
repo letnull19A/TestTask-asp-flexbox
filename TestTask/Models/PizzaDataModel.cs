951dfea949c32923ffa3cc0f95f07c02016092f6
LetNull19A <alexvolkovdd@gmail.com>
2022-11-17 20:51:47 +0400

    3 часть задания

---
+++ TestTask/Models/PizzaDataModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TestTask.Models
+{
+    public sealed class PizzaDataModel
+    {
+        public string? Image { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string[]? Ingredients { get; set; }
+        public int SelectedSize { get; set; }
+        public bool[]? AvailableSizes { get; set; }
+        public int SelectedThiknes { get; set; }
+        public bool[]? AvailableThickness { get; set; }
+        public double Price { get; set; }
+        public double Mass { get; set; }
+        public bool IsHalf { get; set; }
+        public bool IsAvailableThickness { get; set; }
+    }
+}
